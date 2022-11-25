71cccb9f30d23f6b5cabeff562fcadcfd32cfccc
Oleg <olegolegovich@inbox.ru>
2022-11-26 00:08:24 +0300

    added FillArray method

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите количество элементов массива: ");
+int numberOfElements = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[numberOfElements];
+
+FillArray(array);
+
+void FillArray(string[] collection)
+{
+	int length = collection.Length;
+	int index = 0;
+	while (index < length)
+	{
+	    Console.Write("Введите элемент массива: ");
+        string element = Convert.ToString(Console.ReadLine());
+        collection[index] = element;
+        index++;
+	}
+}
