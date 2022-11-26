c5a117e2f87854da2ed5453001fdacb098a57a12
Oleg <olegolegovich@inbox.ru>
2022-11-26 10:27:49 +0300

    Final Version

--- Program.cs
+++ Program.cs
@@ -4,35 +4,59 @@ int numberOfElements = Convert.ToInt32(Console.ReadLine());
 string[] array = new string[numberOfElements];
 
 FillArray(array);
-PrintArray(array);
+string[] result = TrimmedArray(array);
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write($"{result[i]}  ");
+}
 
 // Метод заполняет массив введенными с клавиатуры элементами
 void FillArray(string[] collection)
 {
-	int length = collection.Length;
-	int index = 0;
-	while (index < length)
-	{
-	    Console.Write("Введите элемент массива: ");
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write("Введите элемент массива: ");
         string element = Convert.ToString(Console.ReadLine());
         collection[index] = element;
         index++;
-	}
+    }
 }
 
-// Метод выводит элементы массива, которые не длиннее трех символов
-void PrintArray (string[] col)
+// Метод возвращает элементы массива, которые не длиннее трех символов
+string[] TrimmedArray(string[] col)
 {
-	int count = col.Length;
-	int position = 0;
-	while (position < count)
-	{
+    int count = col.Length;
+    int position = 0;
+    int resultArrayLength = 0;
+
+    // Цикл для подсчета длины результирующего массива
+    while (position < count)
+    {
         int len = col[position].Length;
-		if(len <= 3)
+        if (len <= 3)
         {
-            Console.Write($"{col[position]}  ");
+            resultArrayLength++;
         }
         position++;
-        
-	}
+    }
+
+    position = 0;
+    string[] resultArray = new string[resultArrayLength];
+    int resArrayPosition = 0;
+    
+    // Цикл для переноса значений в результирующий массив
+    while (position < count)
+    {
+        int len = col[position].Length;
+        if (len <= 3)
+        {
+            resultArray[resArrayPosition] = col[position];
+            resArrayPosition++;
+        }
+        position++;
+    }
+    return resultArray;
 }
